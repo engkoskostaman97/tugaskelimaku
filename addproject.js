174d7332c8604df59ccdd663859eaeabbe0820cc
Engkos97 <110719940+engkoskostaman97@users.noreply.github.com>
2022-08-16 01:24:49 +0700

    tambah durasi

--- addproject.js
+++ addproject.js
@@ -44,12 +44,12 @@ function addBlog(event) {
 
   let blog = {
     project,
+    postAt: new Date(),
+    author: "engkos kostaman",
     stardate,
     enddate,
     description,
     image,
-    author: "engkos kostaman",
-    postAt: "17 agustus 2022",
     react,
     node,
     js,
@@ -73,7 +73,10 @@ function renderBlog() {
     document.getElementById("contents").innerHTML +=
       `<div class="addproject">
             <div class="imgproject">
-                <img src="${dataBlog[index].image}" alt="gambar1">
+            <div>
+            <img class="blog-image"src="${dataBlog[index].image}" alt="gambar1">
+            </div>
+               
             </div>
             <div class="judul">
                 <h1>
@@ -83,7 +86,7 @@ function renderBlog() {
             </div>
             <p> ${dataBlog[index].stardate}-${dataBlog[index].enddate}</p>
             <div class="durasi">
-            ${(dataBlog[index].postAt)} | ${dataBlog[index].author}
+            ${getFullTime(dataBlog[index].postAt)} | ${dataBlog[index].author}
             </div>
             <div class="detail">
                 <p>
@@ -102,10 +105,59 @@ function renderBlog() {
                 <button class="btn-edit">Edit Post</button>
                 <button class="btn-post">Post Blog</button>
             </div>
+            <div style="text-align: right; margin-top: 15px">
+            <span style="font-size: 12px; color:grey">${getDistanceTime(dataBlog[index].postAt)}</span>
+        </div>
         </div>
     </div>
         
-      `
+      `;
   }
 }
 
+function getFullTime(time) {
+  let month = ["Januari", "Febuari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
+
+  let date = time.getDate();
+  let monthIndex = time.getMonth();
+  let year = time.getFullYear();
+
+  let hours = time.getHours();
+  let minutes = time.getMinutes();
+
+  if (hours < 10) {
+    hours = "0" + hours;
+  } else if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  let fullTime = `${date} ${month[monthIndex]} ${year} ${hours}:${minutes} WIB`;
+  return fullTime;
+}
+
+function getDistanceTime(time) {
+  let tanggalMulai = new Date();
+  let tanggalSelesai = time;
+
+  let distance = tanggalMulai - tanggalSelesai;
+  console.log(distance);
+
+  let milisecond = 1000;
+  let secondInHours = 3600;
+  let hoursInDay = 24;
+
+  let distanceDay = Math.floor(distance / (milisecond * secondInHours * hoursInDay));
+  let distanceHours = Math.floor(distance / (milisecond * 60 * 60));
+  let distanceMinutes = Math.floor(distance / (milisecond * 60));
+  let distanceSeconds = Math.floor(distance / milisecond);
+
+  if (distanceDay > 0) {
+    return `${distanceDay} day ago`;
+  } else if (distanceHours > 0) {
+    return `${distanceHours} hours ago`;
+  } else if (distanceMinutes > 0) {
+    return `${distanceMinutes} minutes ago`;
+  } else {
+    return `${distanceSeconds} seconds ago`;
+  }
+}
